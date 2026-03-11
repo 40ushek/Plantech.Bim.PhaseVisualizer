39f8b14b1fe4d3e7e94bddbf6f03dc67558f7795
40ushek <40ushek@gmail.com>
2026-03-11 16:52:40 +0100

    Fix Filtered custom property cache consistency

--- Plantech.Bim.Custom/Plugins/Filtered.cs
+++ Plantech.Bim.Custom/Plugins/Filtered.cs
@@ -1,7 +1,5 @@
-using Plantech.Bim.Custom.Configuration;
 using Plantech.Bim.Custom.Services;
 using System.ComponentModel.Composition;
-using Tekla.Structures;
 using Tekla.Structures.CustomPropertyPlugin;
 
 namespace Plantech.Bim.Custom.Plugins;
@@ -10,8 +8,29 @@ namespace Plantech.Bim.Custom.Plugins;
 internal class Filtered : CustomBase, ICustomPropertyPlugin
 {
     private static readonly FilteredEvaluationService EvaluationService = new();
+    private static readonly object SyncRoot = new();
+    private static int _cachedObjectId = -1;
+    private static int _cachedValue = -1;
 
     public double GetDoubleProperty(int objectId) => 0;
-    public int GetIntegerProperty(int objectId) => EvaluationService.Evaluate(objectId).IntegerValue;
-    public string GetStringProperty(int objectId) => string.Empty;
+    public int GetIntegerProperty(int objectId)
+    {
+        lock (SyncRoot)
+        {
+            if (_cachedObjectId == objectId)
+            {
+                return _cachedValue;
+            }
+        }
+
+        var value = EvaluationService.Evaluate(objectId).IntegerValue;
+
+        lock (SyncRoot)
+        {
+            _cachedValue = value;
+            _cachedObjectId = objectId;
+            return _cachedValue;
+        }
+    }
+    public string GetStringProperty(int objectId) => GetIntegerProperty(objectId).ToString();
 }
