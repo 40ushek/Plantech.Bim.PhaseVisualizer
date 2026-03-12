6b78dddba801d96b481232ed0ef98a2989c01080
40ushek <40ushek@gmail.com>
2026-03-12 12:15:56 +0100

    Add Filtered report property stub notes

--- Plantech.Bim.Custom/Plugins/Filtered.cs
+++ Plantech.Bim.Custom/Plugins/Filtered.cs
@@ -1,5 +1,6 @@
 using Plantech.Bim.Custom.Services;
 using System.ComponentModel.Composition;
+using Tekla.Structures;
 using Tekla.Structures.CustomPropertyPlugin;
 
 namespace Plantech.Bim.Custom.Plugins;
@@ -15,6 +16,12 @@ internal class Filtered : CustomBase, ICustomPropertyPlugin
     public double GetDoubleProperty(int objectId) => 0;
     public int GetIntegerProperty(int objectId)
     {
+        //var identifier = new Identifier(objectId);
+        //var modelObject = Common.LazyModelConnector.ModelInstance.SelectModelObject(identifier);
+        //string _class = "";
+        //modelObject.GetReportProperty("CLASS_ATTR", ref _class);
+        //return _class == "2" ? 1 : 0;
+
         lock (SyncRoot)
         {
             if (_cachedObjectId == objectId)
